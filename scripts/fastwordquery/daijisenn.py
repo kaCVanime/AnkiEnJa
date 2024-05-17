82922027dae97de0502c3c049010c01b9656f50a
kaCVanime <kacvanime@gmail.com>
2024-05-17 17:26:38 +0800

    reformat

--- scripts/fastwordquery/daijisenn.py
+++ scripts/fastwordquery/daijisenn.py
@@ -1,44 +1,49 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 from ..base import *
 
-DICT_PATH = u"C:/Users/Administrator/Downloads/Compressed/大辞泉202304/大辞泉202304/DJS.mdx"
+DICT_PATH = (
+    "C:/Users/Administrator/Downloads/Compressed/大辞泉202304/大辞泉202304/DJS.mdx"
+)
 
-@register([u'本地词典-大辞泉', u'大辞泉'])
+
+@register(["本地词典-大辞泉", "大辞泉"])
 class daijisen_mdx(MdxService):
-
     def __init__(self):
         dict_path = DICT_PATH
         # if DICT_PATH is a path, stop auto detect
         if not dict_path:
             from ...service import service_manager, service_pool
+
             for clazz in service_manager.mdx_services:
                 service = service_pool.get(clazz.__unique__)
-                title = service.builder._title if service and service.support else u''
+                title = service.builder._title if service and service.support else ""
                 service_pool.put(service)
-                if title.startswith(u'DJS'):
+                if title.startswith("DJS"):
                     dict_path = service.dict_path
                     break
         super(daijisen_mdx, self).__init__(dict_path)
 
     @property
     def title(self):
-        return getattr(self, '__register_label__', self.unique)
+        return getattr(self, "__register_label__", self.unique)
 
-    @export('accent')
+    @export("accent")
     def meanings(self):
         soup = parse_html(self.get_default_html())
-        accent = soup.find('maccentaudiog')
+        accent = soup.find("maccentaudiog")
         if accent:
             img_tag = accent.a
             img_tag.decompose()
             return accent.get_text()
-        return ''
+        return ""
 
-    @export('表記')
+    @export("表記")
     def hyouki(self):
         soup = parse_html(self.get_default_html())
-        items = list(set(map(lambda t: t.get_text(), soup.find_all('headword', class_="表記"))))
-        result = ''
+        items = list(
+            set(map(lambda t: t.get_text(), soup.find_all("headword", class_="表記")))
+        )
+        result = ""
         if items and len(items) <= 3:
             for item in items:
                 result = result + item
--- scripts/fastwordquery/syougakukanjcv.py
+++ scripts/fastwordquery/syougakukanjcv.py
@@ -1,4 +1,4 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import os
 import re
 import random
@@ -6,12 +6,13 @@ import json
 from ..base import *
 import traceback, pdb, sys
 
-from aqt.qt import debug;
+from aqt.qt import debug
+
 # from BeautifulSoup import BeautifulSoup
 # from bs4 import BeautifulSoup
 
-DICT_PATH = u"C:/Users/Administrator/Downloads/jc/小学馆v3/Shogakukanjcv3.mdx"
-JEV_PATH = u"C:/Users/Administrator/AppData/Roaming/Anki2/addons21/fastwq1/service/dict/jev_list.txt"
+DICT_PATH = "C:/Users/Administrator/Downloads/jc/小学馆v3/Shogakukanjcv3.mdx"
+JEV_PATH = "C:/Users/Administrator/AppData/Roaming/Anki2/addons21/fastwq1/service/dict/jev_list.txt"
 
 
 def add_key(obj, key, value):
@@ -19,32 +20,34 @@ def add_key(obj, key, value):
         obj[key] = value
 
 
-@register([u'本地词典-小学馆日汉v3', u'小学馆日汉v3'])
+@register(["本地词典-小学馆日汉v3", "小学馆日汉v3"])
 class syogakukanjcv(MdxService):
-
     def __init__(self):
         dict_path = DICT_PATH
         # if DICT_PATH is a path, stop auto detect
         if not dict_path:
             from ...service import service_manager, service_pool
+
             for clazz in service_manager.mdx_services:
                 service = service_pool.get(clazz.__unique__)
-                title = service.builder._title if service and service.support else u''
+                title = service.builder._title if service and service.support else ""
                 service_pool.put(service)
-                if title.startswith(u'小学馆日汉v3'):
+                if title.startswith("小学馆日汉v3"):
                     dict_path = service.dict_path
                     break
 
-        self.jev_entries = set(line.strip() for line in open(JEV_PATH, encoding='utf-8'))
+        self.jev_entries = set(
+            line.strip() for line in open(JEV_PATH, encoding="utf-8")
+        )
         super(syogakukanjcv, self).__init__(dict_path)
 
     @property
     def title(self):
-        return getattr(self, '__register_label__', self.unique)
+        return getattr(self, "__register_label__", self.unique)
 
     def _get_html(self):
         html = self.get_html()
-        if html.startswith('@@@LINK='):
+        if html.startswith("@@@LINK="):
             available_entries = [t[8:] for t in html.split()]
             for word in available_entries:
                 if word not in self.jev_entries:
@@ -52,12 +55,16 @@ class syogakukanjcv(MdxService):
                     html = self.get_html()
         return html
 
-    @export('full')
+    @export("full")
     def full(self):
         return self._get_html()
 
     def _find_meaning_tag(self, tag):
-        return tag.name == 'p' and tag.attrs.get("data-orgtag") == "meaning" and not tag.attrs.get("type") == "補足"
+        return (
+            tag.name == "p"
+            and tag.attrs.get("data-orgtag") == "meaning"
+            and not tag.attrs.get("type") == "補足"
+        )
 
     def _get_meanings(self, target, result):
         defs = []
@@ -71,15 +78,15 @@ class syogakukanjcv(MdxService):
 
             def_obj = {}
 
-            '''
+            """
             #fix "置く" def-12.
             <meaning>
             <meaning type="補足">
             <example>
-            '''
+            """
             hosoku_tag = None
             next_sib = meaning.next_sibling
-            if next_sib and next_sib.attrs.get("type") == '補足':
+            if next_sib and next_sib.attrs.get("type") == "補足":
                 hosoku_tag = next_sib
                 hosoku_tag.extract()
                 def_obj["label"] = "[補足]"
@@ -96,7 +103,9 @@ class syogakukanjcv(MdxService):
                         continue
                     else:
                         first_text_node.extract()
-                        def_obj["label"] = def_obj.get("label", "") + first_text_node_text
+                        def_obj["label"] = (
+                            def_obj.get("label", "") + first_text_node_text
+                        )
 
             defs.append(def_obj)
 
@@ -104,11 +113,11 @@ class syogakukanjcv(MdxService):
             for tag in white_squares:
                 tag.extract()
 
-            index_el = meaning.find('b')
+            index_el = meaning.find("b")
             if index_el and index_el.get_text().isnumeric():
                 index_el.extract()
 
-            definition = meaning.find('span', attrs={"type": "語義区分2"})
+            definition = meaning.find("span", attrs={"type": "語義区分2"})
             if definition:
                 definition.extract()
                 def_obj["definition"] = definition.get_text()
@@ -119,8 +128,8 @@ class syogakukanjcv(MdxService):
             for tag in meaning.next_siblings:
                 if tag.get("data-orgtag") == "example":
                     example = {}
-                    ja = tag.find('jae')
-                    cn = tag.find('ja_cn')
+                    ja = tag.find("jae")
+                    cn = tag.find("ja_cn")
                     if ja:
                         example["ja"] = ja.get_text()
                     if cn:
@@ -134,10 +143,14 @@ class syogakukanjcv(MdxService):
 
     def _get_subheads(self, name, target):
         result = []
-        sections = target.find_all('div', attrs={"type": name}, recursive=False)
+        sections = target.find_all("div", attrs={"type": name}, recursive=False)
 
         for item in sections:
-            obj = {"usage": item.find('div', attrs={"data-orgtag": "subheadword"}, recursive=False).get_text()}
+            obj = {
+                "usage": item.find(
+                    "div", attrs={"data-orgtag": "subheadword"}, recursive=False
+                ).get_text()
+            }
             defs = self._get_meanings(item, obj)
             if defs:
                 obj["defs"] = defs
@@ -145,23 +158,25 @@ class syogakukanjcv(MdxService):
 
         return result
 
-    @export('json')
+    @export("json")
     def injson(self):
         try:
             html = self._get_html()
             soup = parse_html(html)
-            entries = soup.find_all('h2', recursive=False) + soup.find_all('h3', recursive=False)
+            entries = soup.find_all("h2", recursive=False) + soup.find_all(
+                "h3", recursive=False
+            )
             result = []
 
             if entries:
                 for entry in entries:
                     obj = {"label": ""}
-                    yomi = entry.find('span', class_='pinyin_h')
+                    yomi = entry.find("span", class_="pinyin_h")
                     if yomi:
                         yomi.extract()
                         obj["yomi"] = yomi.get_text()
                     obj["word"] = entry.get_text()
-                    body = entry.find_next_sibling('section', class_="description")
+                    body = entry.find_next_sibling("section", class_="description")
 
                     add_key(obj, "defs", self._get_meanings(body, obj))
 
@@ -171,7 +186,7 @@ class syogakukanjcv(MdxService):
                     add_key(obj, "idioms", self._get_subheads("慣用句", body))
                     add_key(obj, "compounds", self._get_subheads("複合語", body))
 
-                    if any(key in obj for key in ('defs', 'idioms', 'compounds')):
+                    if any(key in obj for key in ("defs", "idioms", "compounds")):
                         result.append(obj)
             else:
                 compounds = self._get_subheads("複合語", soup)
@@ -179,7 +194,7 @@ class syogakukanjcv(MdxService):
                     result.append({"word": self.word, "compounds": compounds})
 
             if not result:
-                return ''
+                return ""
 
             return json.dumps(result, ensure_ascii=False)
         except:
@@ -187,8 +202,3 @@ class syogakukanjcv(MdxService):
             print(traceback.format_exc())
             traceback.print_exc()
             pdb.post_mortem(tb)
-
-
-
-
-
