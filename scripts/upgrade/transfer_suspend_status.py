321b4e04ea36ec80d4d0d416860411855142c979
kaCVanime <kacvanime@gmail.com>
2024-05-25 19:40:56 +0800

    feat: transfer suspend status

---
+++ scripts/upgrade/transfer_suspend_status.py
@@ -0,0 +1,20 @@
+import logging
+
+from anki_connect import invoke as anki_invoke
+from tqdm import tqdm
+
+
+logger = logging.getLogger(__name__)
+
+
+def run():
+    src_nids = anki_invoke("findCards", query="deck:KEXP::Read")
+    for nid in tqdm(src_nids):
+        if anki_invoke("suspended", card=nid):
+            infos = anki_invoke("cardsInfo", cards=[nid])
+            card = infos[0]
+            target_nids = anki_invoke("findCards", query=f"deck:KEXP::Write id:{card['fields']['id']['value']}")
+            anki_invoke("suspend", cards=target_nids)
+
+if __name__ == "__main__":
+    run()
