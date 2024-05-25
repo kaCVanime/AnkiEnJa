f05e4ff86feb1fe6ea60103ca8b7a57ae5c9c3c4
kaCVanime <kacvanime@gmail.com>
2024-05-25 19:41:06 +0800

    transfer tags

---
+++ scripts/upgrade/transfer_tags.py
@@ -0,0 +1,22 @@
+import logging
+
+from anki_connect import invoke as anki_invoke
+from tqdm import tqdm
+
+from main import get_note_fields
+
+logger = logging.getLogger(__name__)
+
+
+def run():
+    src_nids = anki_invoke("findNotes", query="deck:KEXP::Read -tag:none")
+    for nid in tqdm(src_nids):
+        note = get_note_fields(nid)
+        tags = anki_invoke("getNoteTags", note=nid)
+        target_nids = anki_invoke("findNotes", query=f"deck:KEXP::Write id:{note['id']}")
+        if len(target_nids):
+            anki_invoke("addTags", notes=target_nids, tags=" ".join(tags))
+e
+
+if __name__ == "__main__":
+    run()
