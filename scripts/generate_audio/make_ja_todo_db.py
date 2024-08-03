897f8aa0ad803622749f4b908a0c1a1ae2e3c4e0
kaCVanime <kacvanime@gmail.com>
2024-08-03 15:44:51 +0800

    WIP: filter kanji

--- scripts/generate_audio/make_ja_todo_db.py
+++ scripts/generate_audio/make_ja_todo_db.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from src.utils import anki_invoke, JARecorder
 from tqdm.contrib.concurrent import thread_map
@@ -19,8 +20,71 @@ def get_note_fields(nid):
     note = get_note(nid)
     return {key: value_dict["value"] for key, value_dict in note["fields"].items()}
 
-def convert_examples(examples):
-    return [{"name": e["name"], "ja": e["ja"]} for e in examples]
+# filter uncommon kanji
+def filter_kanji(kanji):
+    if not kanji:
+        return ''
+    kanji = kanji.strip()
+    if kanji.startswith('【'):
+        kanji = kanji[1:-1]
+
+    # Wilhelm Konrad Röntgen
+    if re.search(r"^[a-zA-Z;\d.'・()\- \u0080-\u00ff\u0370-\u03ff\u0100-\u017f]+$", kanji):
+        return ''
+
+    # フランス appliqué
+    if 'フランス ' in kanji or 'ポルトガル ' in kanji or '英 ' in kanji or 'ドイツ ' in kanji or 'アラビア ' in kanji or 'オランダ ' in kanji or 'ヒンディー ' in kanji:
+        return ''
+
+    ks = kanji.split('／')
+    if len(ks) >= 1:
+        ks = kanji.split('┊')
+
+    # ▽: 不常用的读音
+    # ×: 不常用汉字
+    # ＝: 熟字训且不常用汉字
+    common = [k for k in ks if '▽' not in k and '×' not in k and '＝' not in k]
+
+    if not common:
+        return ''
+
+    result = re.sub(r'[×▽－＝]', '', common[0])
+
+    # 挙（げ）句 -> 挙げ句
+    result = re.sub(r'[()（）]', '', result)
+
+    # 現す〔現わす〕 -> 現わす
+    if match := re.search(r'〔(.+?)〕', result):
+        result = match.group(1)
+
+    return result
+
+
+def convert_eg(e, word, kanji):
+    name = e["name"]
+    ja = e["ja"]
+
+    # エッセンス
+    # バニラ-－
+    # 香草精
+    if '-－' in ja:
+        ja = ja.replace('-－', word)
+
+    # 合う
+    # いくつもの川が－・って大河となる
+    # 几条河汇合成为一条大江
+    if '－・' in ja:
+        pass
+
+    if '－' in ja:
+        ja = ja.replace('－', kanji or word)
+
+    return {
+        "name": name,
+        "ja": ja
+    }
+def convert_examples(examples, word, kanji):
+    return [convert_eg(e, word, kanji) for e in examples]
 def update_db(eg_group):
     return recorder.insert_examples(eg_group[0])
 
@@ -34,8 +98,10 @@ def main():
         fields = get_note_fields(nid)
         if fields["examples"]:
             examples = json.loads(fields["examples"])
+            kanji = filter_kanji(fields["kanji"])
+            egs.append([convert_examples(examples, fields["word"], kanji)])
 
-            egs.append([convert_examples(examples)])
+    pass
 
     print('adding to db...')
     thread_map(update_db, egs)
